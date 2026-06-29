01a58539614fa8cfaf9f9be99c5b83bb5bac785f
umesh-1106 <umeshkadari09@gmail.com>
2026-06-29 14:05:49 +0530

    Update camera.py

--- models/camera.py
+++ models/camera.py
@@ -1,1 +1,48 @@
+import cv2
 
+class VideoCamera:
+
+    def __init__(self):
+
+        # Open Webcam
+        self.camera = cv2.VideoCapture(0)
+
+        # Set Camera Resolution
+        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    def __del__(self):
+
+        self.camera.release()
+
+    def get_frame(self):
+
+        success, frame = self.camera.read()
+
+        if not success:
+            return None
+
+        # Encode frame as JPEG
+        ret, buffer = cv2.imencode('.jpg', frame)
+
+        return buffer.tobytes()
+
+
+camera = VideoCamera()
+
+
+def generate_frames():
+
+    while True:
+
+        frame = camera.get_frame()
+
+        if frame is None:
+            break
+
+        yield (
+            b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' +
+            frame +
+            b'\r\n'
+        )
